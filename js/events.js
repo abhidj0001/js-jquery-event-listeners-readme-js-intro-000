a62bd6557c2511c428d488c51d38db2e1cf9f081
Abhishek Jadhav <abhidj0001@gmail.com>
2019-09-24 06:06:51 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -2,6 +2,23 @@
 
 $(document).ready(function(){
 
-// call functions here
+function getIt() {
+  $('p').on('click',function(){
+    alert('Hey!');
+  });
+}
 
+function frameIt() {
+  $('img').on('load',function(){
+    $('img').addClass('tasty');
+  });
+}
+
+function pressIt() {
+  $('input').on('keydown',function(key){
+    if(key.which==71){
+      alert('g was pressed');
+    }
+  });
+}
 });
